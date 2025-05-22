70db1b3e4ccc311f7acc9b99065d84f2fc8d05a5
fg-scw <130990923+fg-scw@users.noreply.github.com>
2025-05-22 18:18:12 +0200

    .

--- variables.tf
+++ variables.tf
@@ -1,40 +1,44 @@
 variable "project_id" {
-    type = string
-    default = "" # A renseigner
+  description = "Scaleway Project ID ciblé (obligatoire)"
+  type        = string
+  default = "" # A Renseigner
 
-    # vous pouvez toujours faire:
-    # export TF_VAR_project_id=$(scw config get default-project-id)
-    # ou `terraform apply -var "project_id=$SCW_PROJECT_ID"`
+  # vous pouvez toujours faire:
+  # export TF_VAR_project_id=$(scw config get default-project-id)
+  # ou `terraform apply -var "project_id=$SCW_PROJECT_ID"`
 }
 
 variable "region" {
-    type    = string
-    default = "fr-par"
+  description = "Région Object Storage (fr-par, nl-ams, pl-war)"
+  type        = string
+  default     = "fr-par"
 }
 
 variable "bucket_name" {
-    type    = string
-    default = "test-scw-sa-fgz"
+  type    = string
+  default = "test-scw-sa-fgz"
 }
 
 variable "write_allowed_ips" {
-    type    = list(string)
-    default = ["0.0.0.0"]
+  description = "IPs/CIDRs autorisées en lecture – vide = pas de filtre"
+  type        = list(string)
+  default     = []
 }
 
 variable "read_allowed_ips" {
-    type    = list(string)
-    default = ["0.0.0.0"]
+  description = "IPs/CIDRs autorisées en lecture – vide = pas de filtre"
+  type        = list(string)
+  default     = []
 }
 
 variable "customer_encryption_key" {
-    description = "Clé AES-256 base64 pour SSE-C (optionnel)"
-    type        = string
-    sensitive   = true
-    default     = ""
+  description = "Clé AES-256 base64 pour SSE-C (optionnel)"
+  type        = string
+  sensitive   = true
+  default     = ""
 }
 
 variable "read_owner_id" {
-    type    = string
-    default = "" # A renseigner avec un user/app_id ayant les droits admin si besoin
+  type    = string
+  default = "db7047ad-0219-4880-a4ee-e790b75fb9d5"
 }
