416b97b8d1945f7077d6128c778e157d5d4d08c0
fg-scw <130990923+fg-scw@users.noreply.github.com>
2025-05-22 18:18:25 +0200

    .

--- versions.tf
+++ versions.tf
@@ -1,20 +1,19 @@
 terraform {
-    required_version = ">= 1.8.0"
+  required_version = ">= 1.8.0"
 
-    required_providers {
-        scaleway = {
-        source  = "scaleway/scaleway"
-        version = ">= 2.54.0"
-        }
-        random = {
-        source  = "hashicorp/random"
-        version = ">= 3.6.0"
-        }
+  required_providers {
+    scaleway = {
+      source  = "scaleway/scaleway"
+      version = ">= 2.54.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.6.0"
+    }
+  }
 }
 
 provider "scaleway" {
-    project_id = var.project_id
-    region     = var.region
-    # Les variables d'environnement SCW_ACCESS_KEY / SCW_SECRET_KEY sont lues automatiquement
+  project_id = var.project_id
+  region     = var.region
 }
